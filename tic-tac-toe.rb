efac0e47d9b57dd61576317dd330ef0393926438
hoarus <hoarus@gmail.com>
2021-10-19 12:31:50 +1000

    Two win conditions configured

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,11 @@
+#We should make the gamesquares a nested array
+# label them 1,2,3
+# Enable spellcheck
+#solve win condition 2
+#make game repeatable
+
 class Board
-  
+  attr_reader :game_over
   def initialize ()
     @name = "gameboard"
     #@a1 = "a-1"
@@ -30,6 +36,7 @@ class Board
       }
     }
     @players = []
+    @game_over = false
   end
 
   def add_player (player)
@@ -53,14 +60,65 @@ class Board
     @turn += 1
     puts "Turn #{@turn}."
     draw_board
-    if @turn.odd?
-      puts "#{@players[0].name}, it is your turn."
-      puts "\nPlease input the space on the board where you would like to place your token."
-      puts "For example, if you wanted to select the bottom-right space, you would input \"c-3\"."
+    @turn.odd? ? player = @players[0] : player = @players[1]
+    puts "#{player.name}, it is your turn."
+    token = " #{player.token} "
+    puts "\nPlease input the space on the board where you would like to place your token."
+    puts "For example, if you wanted to select the bottom-right space, you would input \"c-3\"."
+    valid_input = false
+    while valid_input == false do
       input = gets.chomp.downcase().delete "-"
-      puts @gamesquares[:a1]
-      #Check if input matches any of the board variables)
-      
+      @gamesquares.each do |row, squares|
+        squares.each do |square, value|
+          if value.delete("-") == input
+            squares[square] = token
+            valid_input = true
+          end
+        end
+      end
+      if valid_input == false
+        puts "Invalid input. Please try again:"
+      end
+    end
+    draw_board
+    check_for_win(player, token) 
+  end
+    
+  def check_for_win (player, token)
+    #check all win conditions
+      #if Win condition 1, 2 or 3
+      #that way, stops as soon as anything is true
+
+    #win condition 2
+    #all of column 1, 2 or 3
+
+    #win condition 3
+    #centre piece and
+      #a-1 & c3
+      #c-1 & a-3
+    if win_condition_1(token) || win_condition_3(token)
+      puts "Congratulations #{player.name}, you have won the game!"
+      @game_over = true
+    elsif win_condition_3(token)
+      puts "something"
+    end
+    #return if game_over
+    #game_over = @gamesquares
+  end
+
+  def win_condition_1 (token)
+    return @gamesquares.any? do |rows, squares|
+      squares.all? {|square, value | value == token}
+    end
+  end
+
+  def win_condition_3 (token)
+    if @gamesquares[:row_b][:b2] == token
+      if @gamesquares[:row_a][:a1] == token && @gamesquares[:row_c][:c3] == token
+        return true
+      elsif @gamesquares[:row_c][:c1] == token && @gamesquares[:row_a][:a1] == token
+        return true
+      end
     end
   end
 
@@ -73,7 +131,7 @@ class Player
 
   @@all = []
   
-  def initialize(order, name,token)
+  def initialize(order, name, token)
     @order = order
     @name = name
     @token = token
@@ -97,7 +155,9 @@ gameboard.add_player(player2)
 puts "\n#{player2.name}, you will be using the #{player2.token} token."
 
 #Turn One
+while gameboard.game_over == false do
 gameboard.game_turn
+end
 
 
 
